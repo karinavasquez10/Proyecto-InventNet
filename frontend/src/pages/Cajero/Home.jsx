5b09ec5ef6e847994a3dc4a262d2c371f10b3100
karinavasquez10 <karinavasquez0108@gmail.com>
2025-10-17 21:32:04 -0500

    Guardo mis cambios locales antes de hacer pull

--- frontend/src/pages/Cajero/Home.jsx
+++ frontend/src/pages/Cajero/Home.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+
 // import { useNavigate } from "react-router-dom";
 import {
   Plus,
